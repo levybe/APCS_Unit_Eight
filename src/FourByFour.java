8ac79e5e9211f36331fde93cd78bb4a1a67ed9ca
levybe <levybe@ssfs.org>
2022-02-24 09:44:33 -0500

    Finished all "x By x" programs.

--- src/FourByFour.java
+++ src/FourByFour.java
@@ -1,16 +1,38 @@
 public class FourByFour {
     public static void main(String [] args) {
 //code to define an initially empty int 2D array, minimum size 4x4
-
+        int[][] list = new int[4][4];
 //code to output all original values
+        for (int row = 0; row < list.length; row++) {
+            for (int col = 0; col < list[0].length; col++) {
+                System.out.print(list[row][col] + " ");
+            }
+            System.out.println("");
+        }
 
 //code to change half the values to even positive numbers
+        for (int row = 0; row < list.length; row++) {
+            for (int col = 0; col < list[0].length / 2; col++) {
+                list[row][col] = 2;
+            }
+        }
 
 //and the other half to negative odd numbers
+        for (int row = 0; row < list.length; row++) {
+            for (int col = 3; col >= list[0].length / 2; col--) {
+                list[row][col] = -1;
+            }
+        }
 
 //code to output a blank line separating output sets
-
+        System.out.println("");
 //code to output all array elements
+        for (int row = 0; row < list.length; row++) {
+            for (int col = 0; col < list[0].length; col++) {
+                System.out.print(list[row][col] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
 
--- src/ThreeByThree.java
+++ src/ThreeByThree.java
@@ -1,24 +1,48 @@
 public class ThreeByThree {
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
 //code to define an initially empty double
 //2D array size 3x3
 
-        double [][] list = new double[3][3];
+        double[][] list = new double[3][3];
 
 //code to output all original values
-
+        System.out.print(list[0][0] + " ");
+        System.out.print(list[0][1] + " ");
+        System.out.println(list[0][2]);
+        System.out.print(list[1][0] + " ");
+        System.out.print(list[1][1] + " ");
+        System.out.println(list[1][2]);
+        System.out.print(list[2][0] + " ");
+        System.out.print(list[2][1] + " ");
+        System.out.println(list[2][2]);
 
 //code to change five of the values to positive
-
+        list[0][0] = 17.9;
+        list[0][1] = 23.8;
+        list[0][2] = 1.1;
+        list[1][0] = 62.1;
+        list[1][1] = 23.0;
 
 //and the other four to negative
-
+        list[1][2] = -22.6;
+        list[2][0] = -11.1;
+        list[2][1] = -233.4;
+        list[2][2] = -9.8;
 
 //code to output a blank line separating output sets
-
+        System.out.println("");
 
 //code to output all array elements
+        System.out.print(list[0][0] + " ");
+        System.out.print(list[0][1] + " ");
+        System.out.println(list[0][2]);
+        System.out.print(list[1][0] + " ");
+        System.out.print(list[1][1] + " ");
+        System.out.println(list[1][2]);
+        System.out.print(list[2][0] + " ");
+        System.out.print(list[2][1] + " ");
+        System.out.println(list[2][2]);
+
     }
 }
-
