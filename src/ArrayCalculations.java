81754bf1ebee2f241d7159215f3bd86216b412e2
levybe <levybe@ssfs.org>
2022-03-02 15:11:47 -0500

    Finished ArrayCalculations.

--- src/ArrayCalculations.java
+++ src/ArrayCalculations.java
@@ -1,48 +1,49 @@
 public class ArrayCalculations {
 
     public static int rowSum(int[][] arr, int row) {
-        // create a variable to keep track of the total
+        int total = 0;
 
-
-        /* Check to see if the given row variable is valid. If the row is less than 0
-        or is greater than he number of rows it is not valid. Return -1 if not valid.
-         */
-
-
-        /* if row is valid, create a loop to calculate the sum. hint: elements in a 2D array are found like this:
-        arr[row][column]. If you are finding the sum of a row, the [row] number will not change
-        only the [column] part will.
-         */
-
-
-        // return the total
-        return 0;
+        if (arr.length - 1 >= row && row >= 0) {
+            for (int num : arr[row]) {
+                total += num;
+            }
+            return total;
+        }
+        else {
+            return -1;
+        }
     }
 
     public static int columnSum(int[][] arr, int col) {
-        // create a variable to keep track of the total
+        int total = 0;
 
-
-        /* check to see if the given col variable is valid. If col is less than 0 or is greater than
-        the number of columns (the length of one array) it is not valid. Return -1 if not valid.
-         */
-
-
-        /* if col is valid, create a loop to calculate the sum. hint: elements in a 2D array are found like this:
-        arr[row][column]. If you are finding the sum of a col, the [column] number will not change,
-        only the [row] part will.
-         */
-
-        // return the total
-        return 0;
+        if (arr[0].length - 1 >= col && col >= 0) {
+            for (int i = 0; i < arr.length; i++) {
+                total += arr[i][col];
+            }
+            return total;
+        }
+        else {
+            return -1;
+        }
     }
 
     public static int diagonalSum(int[][] arr, int direction) {
+        int total = 0;
 
-        return 0;
-
+        if (direction == 0) { // If the direction is DOWN
+            for (int a = 0; a < arr.length; a++) {
+                total += arr[a][a];
+            }
+        }
+        else if (direction == 1) { // If the direction is UP
+            for (int a = arr.length - 1; a >= 0; a--) {
+                total += arr[a][(arr.length - 1) - a];
+            }
+        }
+        else { // If an invalid direction is inputted
+            return -1;
+        }
+        return total;
     }
-
-
-
 }
